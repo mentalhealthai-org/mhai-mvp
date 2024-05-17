4673d0e22eec85c739ff9b469138042cd606c3a0
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2024-05-17 13:49:44 -0400

    Add support for multiple langauges

---
+++ src/loveai_mvp/profiles.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Any
+
+import yaml
+
+
+PROFILES_PATH = Path(__file__).parent / "data" / "profiles"
+
+
+def read_profile(file_path) -> dict[str, Any]:
+    """
+    Read the profile data from a YAML file.
+
+    Parameters
+    ----------
+    file_path : str
+        The path to the YAML file.
+
+    Returns
+    -------
+    dict
+        The profile data.
+    """
+    with open(file_path, "r") as file:
+        profile_data = yaml.safe_load(file)
+    return profile_data
+
+
+def get_ai_profile() -> dict[str, Any]:
+    return read_profile(PROFILES_PATH / "ai" / "ai.yaml")
+
+
+def get_user_profile() -> dict[str, Any]:
+    return read_profile(PROFILES_PATH / "users" / "ivan.yaml")
